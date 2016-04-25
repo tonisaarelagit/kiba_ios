80cdc410ba8ce9f90ee7b362cd30b22237033e8d
Toni Saarela <tn.saarela.dev@gmail.com>
2016-04-25 16:21:11 +0200

    initial commit for all project files

---
+++ Kiba/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kiba
+//
+//  Created by admin_user on 4/25/16.
+//  Copyright © 2016 WorldStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kiba/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kiba
+//
+//  Created by admin_user on 4/25/16.
+//  Copyright © 2016 WorldStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
