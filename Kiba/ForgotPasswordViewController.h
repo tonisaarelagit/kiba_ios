436c0ed8dd90a7e2824e30152696a9f787969056
tonisaarelagit <tn.saarela.dev@gmail.com>
2016-05-06 16:08:23 +0200

    add forgot password page

---
+++ Kiba/ForgotPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForgotPasswordViewController.h
+//  Kiba
+//
+//  Created by admin_user on 5/6/16.
+//  Copyright © 2016 WorldStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForgotPasswordViewController : UIViewController
+
+@end
--- Kiba/ProfileViewController.m
+++ Kiba/ProfileViewController.m
@@ -7,13 +7,58 @@
 //
 
 #import "ProfileViewController.h"
+#import "LocalStorage.h"
+#import "Constants.h"
+
+@interface ProfileViewController ()
+{
+    LocalStorage *localStorage;
+}
+
+@end
 
 @implementation ProfileViewController
+{
+    IBOutlet UIButton *btnDeleteAccount;
+    IBOutlet UIImageView *ivProfileImage;
+    IBOutlet UILabel *lblUserName;
+    IBOutlet UILabel *lblEmail;
+}
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
     self.parentViewController.navigationItem.title = @"Profile";
+    
+    UIColor *pink = [UIColor colorWithRed:254 / 255.f green:129 / 255.f blue:89 / 255.f alpha:1.f];
+    btnDeleteAccount.layer.borderColor = pink.CGColor;
+    
+    localStorage = [LocalStorage shared];
+    
+    [self setupUserInfo];
+}
+
+#pragma mark - Setup User Info
+
+- (void)setupUserInfo {
+    lblUserName.text = @"...";
+    lblEmail.text = [localStorage defaultForKey:USER_EMAIL];
+}
+
+#pragma mark - Button Delegate Methods
+
+- (IBAction)didTapLogout:(id)sender {
+    [localStorage removeDefaultForKey:USER_EMAIL];
+    
+    UIViewController *viewController = [self.storyboard instantiateViewControllerWithIdentifier:@"WelcomeViewController"];
+    [self.parentViewController.navigationController setViewControllers:@[viewController] animated:YES];
+}
+
+- (IBAction)didTapDeleteAccount:(id)sender {
+    [localStorage removeDefaultForKey:USER_EMAIL];
+    
+    UIViewController *viewController = [self.storyboard instantiateViewControllerWithIdentifier:@"WelcomeViewController"];
+    [self.parentViewController.navigationController setViewControllers:@[viewController] animated:YES];
 }
 
 @end
